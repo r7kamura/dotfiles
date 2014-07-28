69e946665f6a297423a7a5b0f0565c6ec975c881
Ryo Nakamura <r7kamura@gmail.com>
2014-07-28 22:30:02 +0900

    Refactor installer/uninstaller

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+tempfile=/tmp/dotfiles.zip
+workspace=/tmp/dotfiles
+
 # Download zipped installer
 echo -n "Downloading installer..."
-curl -LSfs -o /tmp/dotfiles.zip https://github.com/r7kamura/dotfiles/archive/master.zip
+curl -LSfs -o ${tempfile} https://github.com/r7kamura/dotfiles/archive/master.zip
 echo "Done"
 
-# Unzip installer into /tmp/dotfiles/dotfiles-master
-unzip -oq /tmp/dotfiles.zip -d /tmp/dotfiles
+# Unzip installer into workspace
+unzip -oq ${tempfile} -d ${workspace}
 
 # Run installer
-/tmp/dotfiles/dotfiles-master/bin/install
+${workspace}/dotfiles-master/bin/install
 
 # Cleanup
-rm -rf /tmp/dotfiles /tmp/dotfiles.zip
+rm -rf ${tempfile} ${workspace}
--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+tempfile=/tmp/dotfiles.zip
+workspace=/tmp/dotfiles
+
 # Download zipped uninstaller
 echo -n "Downloading uninstaller..."
-curl -LSfs -o /tmp/dotfiles.zip https://github.com/r7kamura/dotfiles/archive/master.zip
+curl -LSfs -o ${tempfile} https://github.com/r7kamura/dotfiles/archive/master.zip
 echo "Done"
 
-# Unzip installer into /tmp/dotfiles/dotfiles-master
-unzip -oq /tmp/dotfiles.zip -d /tmp/dotfiles
+# Unzip uninstaller into workspace
+unzip -oq ${tempfile} -d ${workspace}
 
 # Run uninstaller
-/tmp/dotfiles/dotfiles-master/bin/uninstall
+${workspace}/dotfiles-master/bin/uninstall
 
 # Cleanup
-rm -rf /tmp/dotfiles /tmp/dotfiles.zip
+rm -rf ${tempfile} ${workspace}
