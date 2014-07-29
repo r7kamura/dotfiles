962e3495f2d2e0f4deb322a13e8e944b97cf7068
Ryo Nakamura <r7kamura@gmail.com>
2014-07-29 22:17:00 +0900

    Add ghq package

--- lib/dotfiles/packages/ghq.rb
+++ lib/dotfiles/packages/ghq.rb
@@ -2,15 +2,15 @@ module Dotfiles
   module Packages
     class Ghq < Base
       def install
-        system(%<ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" && brew update>)
+        system("brew install ghq")
       end
 
       def installed?
-        system("which brew")
+        system("which ghq")
       end
 
       def uninstall
-        system("brew prune && (ls -A1 $(brew --prefix) | xargs rm -rf) && rm -rf ~/Library/Caches/Homebrew ~/Library/Logs/Homebrew /Library/Caches/Homebrew")
+        system("brew uninstall ghq")
       end
     end
   end
