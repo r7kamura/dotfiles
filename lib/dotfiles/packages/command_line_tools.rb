761e8a8f5d180e763033bbbda55d0b46e489d076
Ryo Nakamura <r7kamura@gmail.com>
2014-07-29 22:09:19 +0900

    Refactor CommandLineTools installation code

--- lib/dotfiles/packages/command_line_tools.rb
+++ lib/dotfiles/packages/command_line_tools.rb
@@ -4,12 +4,19 @@ module Dotfiles
   module Packages
     class CommandLineTools < Base
       def install
-        system("xcode-select --install", out: IO::NULL, err: IO::NULL)
+        prompt
         sleep 5 until installed?
       end
 
       def installed?
         system("xcode-select -p", out: IO::NULL, err: IO::NULL)
+      end
+
+      private
+
+      # Prompt user to install CommandLineTools via GUI
+      def prompt
+        system("xcode-select --install", out: IO::NULL, err: IO::NULL)
       end
     end
   end
