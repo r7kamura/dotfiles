1ed28e776ba4799792b78d2e2b51b1e64de032d2
Ryo Nakamura <r7kamura@gmail.com>
2014-07-30 23:42:33 +0900

    Change LoginShell package from updatable to installable

--- lib/dotfiles/packages/login_shell.rb
+++ lib/dotfiles/packages/login_shell.rb
@@ -1,28 +1,25 @@
 module Dotfiles
   module Packages
     class LoginShell < Base
-      PATH = "/usr/local/bin/zsh"
+      ORIGINAL_SHELL_PATH = "/bin/bash"
+      CUSTOMIZED_SHELL_PATH = "/usr/local/bin/zsh"
 
-      def update
-        unless updated?
-          system(<<-EOS)
-            sudo sh -c "echo #{PATH} >> /etc/shells"
-            chsh -s #{PATH}
-          EOS
-        end
+      def install
+        system(<<-EOS)
+          sudo sh -c "echo #{CUSTOMIZED_SHELL_PATH} >> /etc/shells"
+          chsh -s #{CUSTOMIZED_SHELL_PATH}
+        EOS
+      end
+
+      def installed?
+        ENV["SHELL"] == CUSTOMIZED_SHELL_PATH
       end
 
       def uninstall
         system(<<-EOS)
-          sudo sh -c "sed -e /#{PATH.gsub("/", '\\/')}/d /etc/shells
-          chsh -s /bin/bash
+          sudo sh -c "sed -e /#{CUSTOMIZED_SHELL_PATH.gsub("/", '\\/')}/d /etc/shells
+          chsh -s #{ORIGINAL_SHELL_PATH}
         EOS
-      end
-
-      private
-
-      def updated?
-        ENV["SHELL"] == PATH
       end
     end
   end
