6868772649d93253f4084ebd4c180dfd9720963c
Ryo Nakamura <r7kamura@r7kamura.local>
2014-07-30 22:17:38 +0900

    Fix LoginShell
    LoginShell should be updated after installing zsh

--- lib/dotfiles/packages/login_shell.rb
+++ lib/dotfiles/packages/login_shell.rb
@@ -3,15 +3,13 @@ module Dotfiles
     class LoginShell < Base
       PATH = "/usr/local/bin/zsh"
 
-      def install
-        system(<<-EOS)
-          sudo sh -c "echo #{PATH} >> /etc/shells"
-          chsh -s #{PATH}
-        EOS
-      end
-
-      def installed?
-        ENV["SHELL"] == PATH
+      def update
+        unless updated?
+          system(<<-EOS)
+            sudo sh -c "echo #{PATH} >> /etc/shells"
+            chsh -s #{PATH}
+          EOS
+        end
       end
 
       def uninstall
@@ -19,6 +17,12 @@ module Dotfiles
           sudo sh -c "sed -e /#{PATH.gsub("/", '\\/')}/d /etc/shells
           chsh -s /bin/bash
         EOS
+      end
+
+      private
+
+      def updated?
+        ENV["SHELL"] == PATH
       end
     end
   end
