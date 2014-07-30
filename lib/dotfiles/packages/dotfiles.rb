e372274c8e2fb3ea0ec5bf3870594fb1c932b46e
Ryo Nakamura <r7kamura@gmail.com>
2014-07-31 00:29:01 +0900

    Change dotfiles symlink logic

--- lib/dotfiles/packages/dotfiles.rb
+++ lib/dotfiles/packages/dotfiles.rb
@@ -1,20 +1,27 @@
 module Dotfiles
   module Packages
     class Dotfiles < Base
+      PATH = File.expand_path("~/src/github.com/r7kamura/dotfiles")
+
       def update
-        system(<<-EOS)
-          GOPATH=$HOME ghq get r7kamura/dotfiles
-          GOPATH=$HOME cd $(ghq list -p r7kamura/dotfiles)
-          rake install
-        EOS
+        system("cd #{PATH}/linked && ls -A | grep -v .DS_Store | xargs -I% ln -s $(PWD)/% ~/%")
       end
 
       def installed?
-        true
+        Dir.exist?(PATH)
+      end
+
+      def install
+        system("git clone https://github.com/r7kamura/dotfiles.git #{PATH}")
       end
 
       def uninstall
-        system("rake uninstall")
+        system(<<-EOS)
+          pushd #{PATH}/linked
+          ls -A | xargs -I% rm ~/%
+          popd
+          rm -rf #{PATH}
+        EOS
       end
     end
   end
