e38e9b928e3b73fb904fe3ce564037d47f32a661
Ryo Nakamura <r7kamura@gmail.com>
2014-08-01 20:26:42 +0900

    Add ruby installer

---
+++ lib/dotfiles/packages/ruby.rb
@@ -0,0 +1,26 @@
+module Dotfiles
+  module Packages
+    class Ruby < Base
+      VERSION = "2.1.2"
+
+      def install
+        system(<<-EOS)
+          RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline) \
+            --with-openssl-dir=$(brew --prefix openssl)" rbenv install #{VERSION}
+          rbenv global #{VERSION}
+        EOS
+      end
+
+      def installed?
+        `rbenv versions`.include?(VERSION)
+      end
+
+      def uninstall
+        system(<<-EOS)
+          rbenv uninstall #{VERSION}
+          rbenv global system
+        EOS
+      end
+    end
+  end
+end
