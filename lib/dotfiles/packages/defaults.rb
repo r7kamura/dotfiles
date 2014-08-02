cee8c47bb5ace79a062f4939768a86b0465e3281
Ryo Nakamura <r7kamura@gmail.com>
2014-08-03 01:30:58 +0900

    Update dotfiles repository at update

--- lib/dotfiles/packages/defaults.rb
+++ lib/dotfiles/packages/defaults.rb
@@ -2,7 +2,11 @@ module Dotfiles
   module Packages
     class Defaults < Base
       def update
-        system("bash defaults/update.sh")
+        system(<<-EOS)
+          cd ~/src/github.com/r7kamura/dotfiles
+          git pull
+          bash defaults/update.sh
+        EOS
       end
     end
   end
