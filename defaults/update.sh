0ddd74e88fe5039a5b51bd065580b0b3291d713a
Ryo Nakamura <r7kamura@gmail.com>
2014-08-03 03:26:37 +0900

    Improve the way to update keyboard preferences
    This allows users to update it immediately

--- defaults/update.sh
+++ defaults/update.sh
@@ -16,5 +16,21 @@ killall Dock
 # Enable full-keyboard-access (allowing tab-key to focus on all things)
 defaults write -g AppleKeyboardUIMode -int 3
 
+# Allow access to system preferences from iTerm
+sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db \
+  "INSERT OR REPLACE INTO access VALUES('kTCCServiceAccessibility','com.googlecode.iterm2',0,1,0,NULL);"
+
 # Use function keys as normal
-defaults write -g com.apple.keyboard.fnState -bool true
+# Note: only GUI can force the change without reboot
+osascript <<< '
+tell application "System Preferences"
+    reveal anchor "keyboardTab" of pane "com.apple.preference.keyboard"
+end tell
+tell application "System Events" to tell process "System Preferences"
+    set functionStateCheckbox to checkbox 1 of tab group 1 of window 1
+    tell functionStateCheckbox
+        if not (its value as boolean) then click functionStateCheckbox
+    end tell
+end tell
+quit application "System Preferences"
+'
