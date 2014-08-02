4cc2700163b59148b28d7a2b69ac6595d8d95303
Ryo Nakamura <r7kamura@gmail.com>
2014-08-03 01:18:46 +0900

    Empty the applications in Dock

--- defaults/update.sh
+++ defaults/update.sh
@@ -2,4 +2,9 @@
 set -e
 
 defaults write com.apple.dock autohide -bool true
+
+# Empty the applications persistently shown in Dock
+defaults write com.apple.dock persistent-apps -array
+
+# Reboot Dock to reflect the changes
 killall Dock
