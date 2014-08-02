fda5406cfa00fcc8155432e5017d89d43c685d44
Ryo Nakamura <r7kamura@gmail.com>
2014-08-03 02:38:15 +0900

    Use function keys as normal

--- defaults/update.sh
+++ defaults/update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+# Hide dock automatically
 defaults write com.apple.dock autohide -bool true
 
 # Empty the applications persistently shown in Dock
@@ -8,6 +9,9 @@ defaults write com.apple.dock persistent-apps -array
 
 # Remove Downloads folder from Dock
 defaults write com.apple.dock persistent-others -array
+
+# Use function keys as normal
+defaults write -g com.apple.keyboard.fnState -bool true
 
 # Reboot Dock to reflect the changes
 killall Dock
