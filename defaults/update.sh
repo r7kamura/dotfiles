3a10c0c0ee87d9aa99b41a069a0cf892b0029633
Ryo Nakamura <r7kamura@gmail.com>
2014-08-03 01:56:26 +0900

    Remove Downloads folder from Dock

--- defaults/update.sh
+++ defaults/update.sh
@@ -6,5 +6,8 @@ defaults write com.apple.dock autohide -bool true
 # Empty the applications persistently shown in Dock
 defaults write com.apple.dock persistent-apps -array
 
+# Remove Downloads folder from Dock
+defaults write com.apple.dock persistent-others -array
+
 # Reboot Dock to reflect the changes
 killall Dock
