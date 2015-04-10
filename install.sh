26ec9bc001d94b167734ac2a662b27dc8a8aca80
Ryo Nakamura <r7kamura@gmail.com>
2015-04-11 03:14:44 +0900

    Update installer to install xcode and homebrew

--- install.sh
+++ install.sh
@@ -13,6 +13,26 @@ unzip -oq ${tempfile} -d ${workspace}
 # Move to repository root path
 pushd ${workspace}/dotfiles-master > /dev/null
 
+# Install command-line-tools
+xcode-select -p > /dev/null
+if [ "$?" -ne 0 ]; then
+  xcode-select --install
+  while :
+  do
+    xcode-select -p
+    if [ "$?" -ne 0 ]; then
+      break
+    fi
+    sleep 5
+  done
+fi
+
+# Install homebrew
+which brew > /dev/null
+if [ "$?" -ne 0 ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Install serverkit and its dependencies
 gem install bundler > /dev/null
 bundle install > /dev/null
