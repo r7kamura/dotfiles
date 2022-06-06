747442eb5d092e56b5bc03feb701b43bc5276018
Ryo Nakamura <r7kamura@gmail.com>
2022-06-06 09:46:41 +0900

    Install gh

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ set -euo pipefail
 ln -fs ~/dotfiles/.bashrc ~/.bashrc
 ln -fs ~/dotfiles/.tigrc ~/.tigrc
 
+# https://github.com/cli/cli/blob/866eccc202ec61134e5d7f24234ec1e50d6d2854/docs/install_linux.md
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 apt update
-apt install tig --yes
+apt install --yes \
+  gh \
+  tig
