6249df45b15b36c8743163cd2a4d2706f7a7feec
Ryo Nakamura <r7kamura@gmail.com>
2022-06-06 10:11:21 +0900

    Remove sudo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -fs ~/dotfiles/.tigrc ~/.tigrc
 
 # https://github.com/cli/cli/blob/866eccc202ec61134e5d7f24234ec1e50d6d2854/docs/install_linux.md
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 apt update
 apt install --yes \
