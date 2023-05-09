82a6b1b6f94dfd6a592f41c1a8f339aabb6bdf97
r7kamura <r7kamura@gmail.com>
2023-05-10 03:01:08 +0900

    Add .gitconfig

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 dotfiles_directory="$(cd "$(dirname "$0")"; pwd -P)"
 ln -fs ${dotfiles_directory}/.bashrc ~/.bashrc
+ln -fs ${dotfiles_directory}/.gitconfig ~/.gitconfig
 ln -fs ${dotfiles_directory}/.tigrc ~/.tigrc
 
 sudo apt-get update
