77c775ee4b61194a8276fe37a1c21fda11762738
r7kamura <r7kamura@gmail.com>
2023-05-09 10:05:13 +0900

    Update install.sh for codespaces

--- install.sh
+++ install.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-ln -fs .bashrc ~/.bashrc
-ln -fs .tigrc ~/.tigrc
+dotfiles_directory="$(dirname $0)"
+ln -fs ${dotfiles_directory}/.bashrc ~/.bashrc
+ln -fs ${dotfiles_directory}/.tigrc ~/.tigrc
 
 sudo apt-get update
 sudo apt-get install --yes \
   curl \
   peco \
   tig
-
-# Install the latest gh binary.
-VERSION=`curl "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
-echo $VERSION
-curl -sSL https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_amd64.tar.gz -o gh_${VERSION}_linux_amd64.tar.gz
-tar xvf gh_${VERSION}_linux_amd64.tar.gz
-cp gh_${VERSION}_linux_amd64/bin/gh /usr/local/bin/
 
 echo 'EDITOR="code --wait"' >> ~/.bashrc
